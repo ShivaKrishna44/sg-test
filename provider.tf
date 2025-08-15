3665cb68ea80901387c6fc49f97a6324b7638aa9
cva44 <shivakrishna.vosukula@gmail.com>
2025-08-15 17:30:14 +0530

    sg-test

--- provider.tf
+++ provider.tf
@@ -5,19 +5,14 @@ terraform {
       version = "5.84.0"
     }
   }
-rm -rf .terraform terraform.tfstate terraform.tfstate.backup
 
-
-# (Should match your S3 bucket and DynamoDB table)
- backend "s3" {
-   bucket         = "cva-tf-remote-state"
-   key            = "sg-test"
-   region         = "us-east-1"
-   dynamodb_table = "cva-state-locking"
- }
-
-# 3️⃣ Initialize Terraform fresh with your backend
-
+ # backend "s3" {
+ #  bucket         = "cva-tf-remote-state"
+ #  key            = "sg-test"
+ #  region         = "us-east-1"
+ #  dynamodb_table = "cva-state-locking"
+ # }
+}
 
 provider "aws" {
   # Configuration options
